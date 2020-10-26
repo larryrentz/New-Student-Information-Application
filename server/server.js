75a550e7e67db938e33497abe541658247cdb3f4
swiffle <58474686+swiffle@users.noreply.github.com>
2020-10-26 16:18:15 -0400

    Add files via upload
    Small update to develop branch, includes JSON file for the links and basic routing for different paths in server

--- server/server.js
+++ server/server.js
@@ -12,3 +12,23 @@ app.all('/*', (req, res) => {
 });
 
 app.listen(config.port, () => console.log(`App now listening on port ${config.port}`));
+
+app.get('/map', (req, res) => {
+    res.render('index.html')
+})
+
+app.get('/academics', (req, res) => {
+    res.render('index.html')
+})
+
+app.get('/health', (req, res) => {
+    res.render('index.html')
+})
+
+app.get('/housing', (req, res) => {
+    res.render('index.html')
+})
+
+app.get('/finance', (req, res) => {
+    res.render('index.html')
+})
