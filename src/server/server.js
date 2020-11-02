f96466ed682ad5aaf2274b11115c3a04d4d91602
Tom Liraz <tomliraz@gmail.com>
2020-11-02 17:56:36 -0500

    Merged the Google Map code into the Front End app.

--- src/server/server.js
+++ src/server/server.js
@@ -15,8 +15,6 @@ const db = connectToDatabase().on(
 
 const app = express();
 
-app.use('/', express.static('./../../client'));
-
 app.all('/*', (req, res) => {
  res.statusCode === 404 ? res.send('Sorry, information not available') : res.sendFile(path.resolve('./client/index.html'))  
 });
