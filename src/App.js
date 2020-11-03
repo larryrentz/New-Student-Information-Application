a2990da5eef249033fab4b9379725609f340f89a
loedge <36237785+loedge@users.noreply.github.com>
2020-11-03 15:49:42 -0500

    Academics and styling
    Put links in the Academics page and changed the layout of the health page. added a border around the hotline bar. Made other minor styling changes including reducing the size of the top navigation bar.

--- src/App.js
+++ src/App.js
@@ -111,8 +111,48 @@ function Home() {
 
 function Academics() {
   return (
-    <div>
-      <h1>About</h1>
+    <div classname="AcademicsPage">
+      <div class="column">
+        <h1>Undergraduate Resources</h1>
+        <div class="column">
+          <h2>Advising</h2>
+          <p><a href="http://www.ufadvising.ufl.edu/college-remote-advising-contacts/"
+          target="_blank" rel="noopener noreferrer">College Remote Advising Center</a></p>
+        </div>
+        <div class="column">
+          <h2>Tutoring</h2>
+          <p><a href="https://teachingcenter.ufl.edu/tutoring/" target="_blank"
+          rel="noopener noreferrer">Teaching Center</a></p>
+        </div>
+        <div class="column">
+          <h2>Job Resources</h2>
+          <p><a href="https://career.ufl.edu/"
+          target="_blank" rel="noopener noreferrer">Career Center</a></p>
+        </div>
+        <div class="column">
+          <h2>Honors Programs</h2>
+          <p><a href="http://www.honors.ufl.edu/"
+          target="_blank" rel="noopener noreferrer">Honors Program</a></p>
+        </div>
+        <div class="column">
+          <h2>Admissions</h2>
+          <p><a href="https://admissions.ufl.edu/apply/transfer/"
+          target="_blank" rel="noopener noreferrer">Transfer Student Center</a></p>
+        </div>
+        <div class="column">
+          <h2>Scholarships</h2>
+          <p><a href="https://cur.aa.ufl.edu/scholarships/"
+          target="_blank" rel="noopener noreferrer">Center for Undergraduate Research</a></p>
+        </div>
+      </div>
+      <div class="column">
+        <h1>Graduate Resources</h1>
+        <div class="column">
+          <h2>Graduate Resources</h2>
+          <p><a href="http://graduateschool.ufl.edu/prospective-students/funding/fellowships/"
+          target="_blank" rel="noopener noreferrer">Fellowships Center</a></p>
+        </div>
+      </div>
     </div>
   );
 }
@@ -131,7 +171,7 @@ function Finance() {
         <div>
       <div class="header">
         {/* do not change ^ to header */}
-        <h1>In an emergency call 911   For mental distress call the suicde hotline at 1-800-273-8255</h1>
+        <h1>In an emergency call 911.   For mental distress call the suicde hotline at 1-800-273-8255</h1>
       </div>
 
       <div class="ui divider"></div>
