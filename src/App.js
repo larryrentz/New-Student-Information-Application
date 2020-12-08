c7a9a6972e6096069b5d62bfd407057a4812a8cc
Bronte Cadogan <brontecadogan@ufl.edu>
2020-12-08 13:01:36 -0500

    Added Login Functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect,
+  withRouter
 } from "react-router-dom";
 import Home from './Components/Home.js';
 import Academics from './Components/Academics.js';
@@ -12,28 +14,116 @@ import Finance from './Components/Finance.js';
 import Health from './Components/Health.js';
 import Housing from './Components/Housing.js';
 import GoogleMap from './Components/GoogleMap.js';
+import GoogleBtn from './Components/GoogleBtn';
 
+
+
+
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+require('dotenv').config();
+
+
+const {
+  REACT_APP_GOOGLE_LOGIN_API_KEY
+} = process.env;
+
+
+const CLIENT_ID = `${REACT_APP_GOOGLE_LOGIN_API_KEY}`;
+//import Login from './Components/sss.js'
 // TODO Add a 404 page
 // TODO commit package.json file
 // TODO make the homepage the default first page
 // TODO! MORTGAGE.JS WAS INSTALLED, react-compound-interestwas installed, interest.js installed, Passport.js installed
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100) // fake async
+  }
+}
+
+//const Public = () => <h3>Public</h3>
+//const Protected = () => <h3>Protected</h3>
+
+class Login extends React.Component {
+        state = {
+          redirectToReferrer: false
+        }
+        login = () => {
+          fakeAuth.authenticate(() => {
+            this.setState(() => ({
+              redirectToReferrer: true
+            }))
+          })
+        }
+        render() {
+          const { from } = this.props.location.state || { from: { pathname: '/' } }
+          const { redirectToReferrer } = this.state
+
+          if (redirectToReferrer === true) {
+            return <Redirect to={from} />
+          }
+
+          return (
+            <div>
+                <div>
+                    { this.state.isLogined ?
+                      <GoogleLogout
+                        clientId={ CLIENT_ID }
+                        buttonText='Logout'
+                        onLogoutSuccess={ this.logout }
+                        onFailure={ this.handleLogoutFailure }
+                      >
+                      </GoogleLogout>: <GoogleLogin
+                        clientId={ CLIENT_ID }
+                        buttonText='Login'
+                        onSuccess={ this.login }
+                        onFailure={ this.handleLoginFailure }
+                        cookiePolicy={ 'single_host_origin' }
+                        responseType='code,token'
+                      />
+                    }
+                    </div>
+              
+            </div>
+          )
+        }
+}
+
+
+const AuthButton = withRouter(({ history }) => (
+  fakeAuth.isAuthenticated
+    ? <p>
+        Welcome! <button onClick={() => {
+          fakeAuth.signout(() => history.push('/'))
+        }}>Sign out</button>
+      </p>
+    : <p>You are not logged in.</p>
+))
+
+
+//Write Private Route instead of Route if you want to restrict access
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to={{
+          pathname: '/login',
+          state: { from: props.location }
+        }} />
+  )} />
+)
 
 export default function App() {
   return (
     <Router>
-      {/* ADD Login functionality
-      <div>
-        <Link to="/log_in">Login</Link>
-      </div>*/}
+      
       <div>
         <div class="topnav">
           <a class="active" href="#logo">UF</a>
@@ -79,19 +169,41 @@ export default function App() {
 
           NOTE TO SELF GET RID OF EXACT makes it only one homepage
         */}
+
+
+        <div>
+        <AuthButton />
+        </div>
+      
+{/*
+        <ul>
+          <li><Link to="/public">Public Page</Link></li>
+          <li><Link to="/protected">Protected Page</Link></li>
+        </ul>
+
+        <Route path="/public" component={Public} />
+        you must login to see this page plus the login 
+        <Route path="/login" component={Login} />
+        <PrivateRoute path='/protected' component={Protected} />
+      </div>
+      */}
+
         <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/home" component={Home}/>
-          <Route path="/log_in">
+        <Route path="/login" component={Login} />
+          <PrivateRoute exact path="/" component={Home}/>
+          <PrivateRoute path="/home" component={Home}/>
+          {/* <Route path="/log_in">
             <log_in />
-          </Route>
-          <Route path="/academics" component={Academics}/>
-          <Route path="/finance" component={Finance}/>
-          <Route path="/health" component={Health}/>
-          <Route path="/housing" component={Housing}/>
-          <Route path="/map" component={GoogleMap}>
-            <GoogleMap />
-          </Route>
+          </Route> */}
+        
+         
+          <PrivateRoute path="/academics" component={Academics}/>
+          <PrivateRoute path="/finance" component={Finance}/>
+          <PrivateRoute path="/health" component={Health}/>
+          <PrivateRoute path="/housing" component={Housing}/>
+          <PrivateRoute path="/map" component={GoogleMap}>
+          {/*<GoogleMap /> leftover artifact, not necessary*/}
+          </PrivateRoute>
         </Switch>
     </Router>
   );
--- src/Components/GoogleBtn.js
+++ src/Components/GoogleBtn.js
@@ -12,72 +12,72 @@ const CLIENT_ID = `${REACT_APP_GOOGLE_LOGIN_API_KEY}`;
 
 
 class GoogleBtn extends Component {
-   constructor(props) {
-    super(props);
+                constructor(props) {
+                  super(props);
 
-    this.state = {
-      isLogined: false,
-      accessToken: ''
-    };
+                  this.state = {
+                    isLogined: false,
+                    accessToken: ''
+                  };
 
-    this.login = this.login.bind(this);
-    this.handleLoginFailure = this.handleLoginFailure.bind(this);
-    this.logout = this.logout.bind(this);
-    this.handleLogoutFailure = this.handleLogoutFailure.bind(this);
-  }
+                  this.login = this.login.bind(this);
+                  this.handleLoginFailure = this.handleLoginFailure.bind(this);
+                  this.logout = this.logout.bind(this);
+                  this.handleLogoutFailure = this.handleLogoutFailure.bind(this);
+                }
 
-  login (response) {
-    if(response.accessToken){
-      this.setState(state => ({
-        isLogined: true,
-        accessToken: response.accessToken
-      }));
-    }
-  }
+                login (response) {
+                  if(response.accessToken){
+                    this.setState(state => ({
+                      isLogined: true,
+                      accessToken: response.accessToken
+                    }));
+                  }
+                }
 
-  logout (response) {
-    this.setState(state => ({
-      isLogined: false,
-      accessToken: ''
-    }));
-  }
+                logout (response) {
+                  this.setState(state => ({
+                    isLogined: false,
+                    accessToken: ''
+                  }));
+                }
 
-  handleLoginFailure (response) {
-    alert('Failed to log in')
-  }
+                handleLoginFailure (response) {
+                  alert('Failed to log in')
+                }
 
-  handleLogoutFailure (response) {
-    alert('Failed to log out')
-  }
+                handleLogoutFailure (response) {
+                  alert('Failed to log out')
+                }
 
 
- 
-  render() {
-    return (
-    <div>
-      { this.state.isLogined ?
-        <GoogleLogout
-          clientId={ CLIENT_ID }
-          buttonText='Logout'
-          onLogoutSuccess={ this.logout }
-          onFailure={ this.handleLogoutFailure }
-        >
-        </GoogleLogout>: <GoogleLogin
-          clientId={ CLIENT_ID }
-          buttonText='Login'
-          onSuccess={ this.login }
-          onFailure={ this.handleLoginFailure }
-          cookiePolicy={ 'single_host_origin' }
-          responseType='code,token'
-        />
-      }
-{/*shows you the access token */}   
-<div style={{ marginLeft: "-1800px" ,paddingTop: "0%", paddingBottom: "60%" , paddingLeft: "100%"}} >
- { this.state.accessToken ? <h6>Your User Key is :{ this.state.accessToken }</h6> : null }
- </div>
-    </div>
-    )
-  }
+              
+                render() {
+                  return (
+                  <div>
+                    { this.state.isLogined ?
+                      <GoogleLogout
+                        clientId={ CLIENT_ID }
+                        buttonText='Logout'
+                        onLogoutSuccess={ this.logout }
+                        onFailure={ this.handleLogoutFailure }
+                      >
+                      </GoogleLogout>: <GoogleLogin
+                        clientId={ CLIENT_ID }
+                        buttonText='Login'
+                        onSuccess={ this.login }
+                        onFailure={ this.handleLoginFailure }
+                        cookiePolicy={ 'single_host_origin' }
+                        responseType='code,token'
+                      />
+                    }
+              {/*shows you the access token */}   
+              <div style={{ marginLeft: "-1800px" ,paddingTop: "0%", paddingBottom: "60%" , paddingLeft: "100%"}} >
+              { this.state.accessToken ? <h6>Your User Key is :{ this.state.accessToken }</h6> : null }
+              </div>
+                  </div>
+                  )
+                }
 }
 
 export default GoogleBtn;
